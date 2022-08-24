33b1986f351611d3db21be0803df89710f402f47
pig <dudgh5184@naver.com>
2022-08-24 23:30:28 +0900

    ~#3.6

--- src/server.js
+++ src/server.js
@@ -4,8 +4,29 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleWare = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not here</h1>");
+  }
+  console.log("ok go");
+  next();
+};
+
+app.use(logger);
+app.use(privateMiddleWare);
+
 app.get("/", (req, res) => {
-  return res.send("wow");
+  return res.end();
+});
+
+app.get("/protected", (req, res) => {
+  return res.send("welcome");
 });
 
 const handleListenging = () => {
