ed708c28f485818c936802eadd5affb72320c9d6
yh lee <pigmon@mainpc.localdomain>
2022-08-30 23:42:03 +0900

    ~#4.1

---
+++ #README.md
@@ -0,0 +1,15 @@
+# Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete -> delete user
+
+/videos/watch -> watch video
+/videos/edit -> edit video
+/videos/delete -> delete video
+/videos/comments -> comment on a video
+/videos/commnets/delete -> delete a comment of a video
--- src/server.js
+++ src/server.js
@@ -1,34 +1,61 @@
 import express from "express";
+import morgan from "morgan";
 
+// 포트번호
 const PORT = 4000;
 
+// express 호출
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleWare = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not here</h1>");
-  }
-  console.log("ok go");
-  next();
-};
-
+// middlewere
 app.use(logger);
-app.use(privateMiddleWare);
 
-app.get("/", (req, res) => {
-  return res.end();
-});
+// router start
 
-app.get("/protected", (req, res) => {
-  return res.send("welcome");
-});
+// global
+const globalRouter = express.Router();
 
+// funtion
+const handleHome = (req, res) => {
+  res.send("home");
+};
+
+// get
+globalRouter.get("/", handleHome);
+
+//
+// user
+const userRouter = express.Router();
+
+// funtion
+const handleEditUser = (req, res) => {
+  res.send("user");
+};
+
+// get
+userRouter.get("/edit", handleEditUser);
+
+//
+// video
+const videoRouter = express.Router();
+
+// funtion
+const handleWatchVideo = (req, res) => {
+  res.send("video");
+};
+
+// get
+videoRouter.get("/watch", handleWatchVideo);
+
+// router end
+
+// use router
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+// 연결
 const handleListenging = () => {
   console.log(`server on port http://localhost:${PORT}`);
 };
