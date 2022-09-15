8de740752cd560ed9bb77fb59f230ac68403f3b2
pigmon <dudgh5184@naver.com>
2022-09-15 23:10:09 +0900

    ~#5.2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,12 +2,10 @@ export const trending = (req, res) => {
   res.render("home");
 };
 export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`watch video ${req.params.id}`);
+  res.render("watch");
 };
 export const edit = (req, res) => {
-  console.log(req.params);
-  res.send(`eidt video ${req.params.id}`);
+  res.render("edit");
 };
 export const search = (req, res) => {
   res.send("search");
