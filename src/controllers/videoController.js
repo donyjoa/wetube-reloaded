b360bd1cc1c7dac7be75e493b6f196dc46da4c31
pigmon <dudgh5184@naver.com>
2022-10-11 23:36:12 +0900

    ~#6.1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,37 @@
+let videos = [
+  {
+    title: "HI",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 min ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "wow",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 min ago",
+    views: 50,
+    id: 2,
+  },
+  {
+    title: "good",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 min ago",
+    views: 50,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "HI",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 min ago",
-      views: 50,
-      id: 1,
-    },
-    {
-      title: "wow",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 min ago",
-      views: 50,
-      id: 1,
-    },
-    {
-      title: "good",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 min ago",
-      views: 50,
-      id: 1,
-    },
-  ];
   res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
-  res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watch ${video.title}`, video });
 };
 export const edit = (req, res) => {
   res.render("edit");
