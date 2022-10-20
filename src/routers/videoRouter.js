158322e02d401fa1a3c6eef5fecaeeb250261686
pigmon <dudgh5184@naver.com>
2022-10-20 20:45:34 +0900

    //

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -16,7 +16,7 @@ videoRouter.get("/:id(\\d+)", watch);
 // edit
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
-/*
+/* 구버전
 videoRouter.get("/:id(\\d+)/edit", getEdit);
 videoRouter.post("/:id(\\d+)/edit", postEdit);
 */
