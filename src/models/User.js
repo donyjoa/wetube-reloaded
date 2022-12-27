78dbfe6387d35bcff4bc0d7ba28ed8aa47058eb1
pig <pigmon1997@gmail.com>
2022-12-27 23:27:32 +0900

    ~#7.21

--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
-  password: { type: String, require: true },
+  password: { type: String, require: false },
   name: { type: String, required: true },
   location: String,
 });
